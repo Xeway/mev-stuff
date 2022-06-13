39ae9e589e03a35ddbf9bda19eb1f24a271bd6f5
Ewan Humbert <xeway@protonmail.com>
2022-06-13 11:10:38 +0200

    Created the base for arbitrage contract

--- on-chain/contracts/Arbitrage.sol
+++ on-chain/contracts/Arbitrage.sol
@@ -5,6 +5,8 @@ import "../external_contracts/aave-v3-core/contracts/flashloan/base/FlashLoanSim
 // import "../external_contracts/aave-v3-core/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
 // import "../external_contracts/aave-v3-core/contracts/interfaces/IPool.sol";
 
+import "./interfaces/IUniswapV2Router02.sol";
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Arbitrage is FlashLoanSimpleReceiverBase {
@@ -12,8 +14,24 @@ contract Arbitrage is FlashLoanSimpleReceiverBase {
     FlashLoanSimpleReceiverBase(_providerAddress)
     public {}
 
-    function performArbitrage(address _pair, address _token, uint _amount) public {
-        POOL.flashLoanSimple(address(this), _token, _amount, bytes(""), 0);
+    function performArbitrage(
+        address _tokenToBorrow,
+        uint _amountToBorrow,
+        address _tokenToReceive,
+        uint _amountToReceiveMin,
+        address _routerAddressToBorrow,
+        address _routerAddressToReceive
+    ) public {
+        bytes parameters = abi.encode(
+            _tokenToBorrow,
+            _amountToBorrow,
+            _tokenToReceive,
+            _amountToReceiveMin,
+            _routerAddressToBorrow,
+            _routerAddressToReceive
+        );
+
+        POOL.flashLoanSimple(address(this), _tokenToBorrow, _amountToBorrow, parameters, 0);
     }
 
     function executeOperation(
@@ -24,10 +42,42 @@ contract Arbitrage is FlashLoanSimpleReceiverBase {
         bytes calldata params
     ) external override returns (bool) {
         // arbitrage code will be here
+        (
+            _tokenToBorrow,
+            _amountToBorrow,
+            _tokenToReceive,
+            _amountToReceiveMin,
+            _routerAddressToBorrow,
+            _routerAddressToReceive
+        ) = abi.decode(params, (address, uint, address, uint, address, address));
+        
+        // first swap
+        _swap(_tokenToBorrow, _amountToBorrow, _tokenToReceive, _amountToReceiveMin, _routerAddressToBorrow);
+        // at this point, we've received at least _amountToReceive token
+
+        // second swap
+        _swap(_tokenToReceive, _amountToReceiveMin, _tokenToBorrow, _amountToBorrow, _amountToBorrow, _routerAddressToReceive);
 
         uint amountOwing = amount + premium;
         IERC20(asset).approve(address(POOL), amountOwing);
 
         return true;
+    }
+
+    function _swap(address _tokenIn, uint _amountIn, address _tokenOut, uint _amountOutMin, address _routerAddress) internal {
+        IERC20(_tokenIn).approve(_routerAddress, _amountIn);
+
+        address[] memory path = new address[](2);
+        path[0] = _tokenIn;
+        path[1] = _tokenOut;
+
+        IUniswapV2Router02(_routerAddress)
+        .swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            _amountIn,
+            _amountOutMin,
+            path,
+            address(this),
+            block.timestamp
+        );
     }
 }
