d06d49444a1122198de7e6252217fb164dd435a7
Ewan Humbert <xeway@protonmail.com>
2022-08-17 22:14:29 +0200

    Added block security

--- off-chain/main.go
+++ off-chain/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
@@ -8,24 +9,41 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func main() {
-	start := time.Now()
-	// Create a client instance to connect to our provider
-	client, err := ethclient.Dial("https://eth-mainnet.alchemyapi.io/v2/YgladcqcW2iHbKZVTYd4S0VFcESU4UKw")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
+func EvaluateArb(client *ethclient.Client) {
 	amount := int64(1)
 
 	graph := modules.GetAllRates(client, amount)
 	bestPath := modules.FindBestPath(graph)
 
 	fmt.Println(graph, "\n\n\n", bestPath)
-	fmt.Println(time.Since(start))
 
 	// amountsOut := modules.GetAllUniswapAmountOut(client, amount)
 	// bestOpportunity := modules.GetBestArbitrageOpportunity(client, amountsOut)
 	// fmt.Println(bestOpportunity)
+}
+
+func main() {
+	start := time.Now()
+
+	// Create a client instance to connect to our provider
+	client, err := ethclient.Dial("wss://mainnet.infura.io/ws/v3/7f241e16d45245599aedb55e901250c2")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	currentHeader, _ := client.HeaderByNumber(context.Background(), nil)
+	currentBlock := currentHeader.Number.Int64()
+
+	EvaluateArb(client)
+
+	latestHeader, _ := client.HeaderByNumber(context.Background(), nil)
+	latestBlock := latestHeader.Number.Int64()
+	// if the block at the beginning of the computation is different from the block after the computation
+	// it means that data changed and therefore arbitrage opportunity can be biased
+	// so in this case we go the computation again
+	if currentBlock != latestBlock {
+		EvaluateArb(client)
+	}
+
+	fmt.Println(time.Since(start))
 }
